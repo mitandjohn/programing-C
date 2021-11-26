a11364f46d2fbe801ce4ec9b308e0ed2eb7637a8
Minh Phung <minhph1976@yahoo.com>
2021-11-26 20:34:44 +1300

    hw1

--- homework/hw001/code4.c
+++ homework/hw001/code4.c
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 int main(){
-    float x = 1.0;
-    float y = 2.0;
-    printf("if x= %f and y=%f then Y^6+X.Y^5+X^2.Y^4+X^3.Y^3+X^4.Y^2+X^5.Y+X^6 = %f",x, y, pow(y,6)+x*pow(y,5)+pow(x,2)*y+pow(x,4)*pow(y,3)+pow(x,4));
+    float x = 1.f; //<-- so co the chuyen duoc
+    float y = 2.f; //<-- so co the chuyen duoc
+    printf("if x= %f and y=%f then Y^6+X.Y^5+X^2.Y^4+X^3.Y^3+X^4.Y^2+X^5.Y+X^6 = %f",x, y, pow(y,6)+x*pow(y,5)+pow(x,2)*pow(y,4)+pow(x,3)+pow(y,3)+pow(x,4)*(y,2)+pow(x,5)*y+(x,6));
 }
